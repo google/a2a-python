1da4b3bd35c5ebb25baf2a35861f66ed4dbb2486
martimfasantos <72747170+martimfasantos@users.noreply.github.com>
2025-05-15 16:31:44 +0100

    add-fastapi-application

--- src/a2a/server/apps/__init__.py
+++ src/a2a/server/apps/__init__.py
@@ -1,5 +1,12 @@
 from a2a.server.apps.http_app import HttpApp
+from a2a.server.apps.default_app import DefaultA2AApplication
 from a2a.server.apps.starlette_app import A2AStarletteApplication
+from a2a.server.apps.fastapi_app import A2AFastAPIApplication
 
 
-__all__ = ['A2AStarletteApplication', 'HttpApp']
+__all__ = [
+    'DefaultA2AApplication',
+    'A2AStarletteApplication', 
+    'A2AFastAPIApplication', 
+    'HttpApp'
+]
