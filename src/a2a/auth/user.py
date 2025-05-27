45202a54a3ec6b3d12f5d72bfd864e657e771c9e
Mike Smith <mikeas@google.com>
2025-05-27 16:43:35 +0000

    Introduce simple server request auth structures

--- src/a2a/auth/user.py
+++ src/a2a/auth/user.py
@@ -1,4 +1,29 @@
-class User:
+"""Authenticated user information."""
+
+from abc import ABC, abstractmethod
+
+
+class User(ABC):
     """A representation of an authenticated user."""
-    def __init__(self):
-        pass
+
+    @abstractmethod
+    @property
+    def is_authenticated(self) -> bool:
+        """Returns whether the current user is authenticated."""
+
+    @abstractmethod
+    @property
+    def user_name(self) -> str:
+        """Returns the user name of the current user."""
+
+
+class UnauthenticatedUser(User):
+    """A representation that no user has been authenticated in the request."""
+
+    @property
+    def is_authenticated(self):
+        return False
+
+    @property
+    def user_name(self) -> str:
+        return ''
--- src/a2a/server/context.py
+++ src/a2a/server/context.py
@@ -3,6 +3,8 @@
 import collections.abc
 import typing
 
+from a2a.auth.user import UnauthenticatedUser, User
+
 
 State = collections.abc.MutableMapping[str, typing.Any]
 
@@ -13,10 +15,14 @@ class ServerCallContext:
     This class allows storing arbitrary user data in the state attribute.
     """
 
-    def __init__(self, state: State | None = None):
-        if state is None:
-            state = {}
-        self._state = state
+    def __init__(self, state: State | None = None, user: User | None = None):
+        self._state = state or {}
+        self._user = user or UnauthenticatedUser()
+
+    @property
+    def user(self) -> User:
+        """Get the user associated with this context, or UnauthenticatedUser."""
+        return self._user
 
     @property
     def state(self) -> State:
