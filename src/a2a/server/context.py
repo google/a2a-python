8e11ddcb6750619b174d515a89b6cdd9cd6b4e49
Mike Smith <mikeas@google.com>
2025-05-27 16:53:15 +0000

    Make ServerCallContext a pydantic model

--- src/a2a/server/context.py
+++ src/a2a/server/context.py
@@ -3,28 +3,19 @@
 import collections.abc
 import typing
 
+from pydantic import BaseModel, Field
+
 from a2a.auth.user import UnauthenticatedUser, User
 
 
 State = collections.abc.MutableMapping[str, typing.Any]
 
 
-class ServerCallContext:
+class ServerCallContext(BaseModel):
     """A context passed when calling a server method.
 
     This class allows storing arbitrary user data in the state attribute.
     """
 
-    def __init__(self, state: State | None = None, user: User | None = None):
-        self._state = state or {}
-        self._user = user or UnauthenticatedUser()
-
-    @property
-    def user(self) -> User:
-        """Get the user associated with this context, or UnauthenticatedUser."""
-        return self._user
-
-    @property
-    def state(self) -> State:
-        """Get the user-provided state."""
-        return self._state
+    state: State = Field(default={})
+    user: User = Field(default=UnauthenticatedUser())
