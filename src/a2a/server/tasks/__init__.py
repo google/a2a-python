dd226447ce0ee5071a5a2b5e7c21d3245b204e27
Krishna Thota <kcthota@google.com>
2025-07-13 21:35:35 -0700

    fix: Resolve dependency issue with sql stores

--- src/a2a/server/tasks/__init__.py
+++ src/a2a/server/tasks/__init__.py
@@ -1,9 +1,10 @@
 """Components for managing tasks within the A2A server."""
 
+import logging
+
 from a2a.server.tasks.base_push_notification_sender import (
     BasePushNotificationSender,
 )
-from a2a.server.tasks.database_task_store import DatabaseTaskStore
 from a2a.server.tasks.inmemory_push_notification_config_store import (
     InMemoryPushNotificationConfigStore,
 )
@@ -17,6 +18,18 @@ from a2a.server.tasks.task_manager import TaskManager
 from a2a.server.tasks.task_store import TaskStore
 from a2a.server.tasks.task_updater import TaskUpdater
 
+
+log = logging.getLogger(__name__)
+
+try:
+    from a2a.server.tasks.database_task_store import DatabaseTaskStore
+except ImportError as e:
+    # If the database task store is not available, we can still use in-memory stores.
+    log.debug(
+        'DatabaseTaskStore not loaded. This is expected if database dependencies are not installed. Error: %s',
+        e,
+    )
+    DatabaseTaskStore = None
 
 __all__ = [
     'BasePushNotificationSender',
